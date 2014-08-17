aa4c8bf502a923211b12f049616b3dd9f194708f
Brain Leke <bleke@thoughtworks.com>
2014-08-17 12:15:10 +0200

    Brain: Modifying the grunt packages and adding some package files

---
+++ test/karma-unit.conf.js
@@ -0,0 +1,15 @@
+var sharedConfig = require('./karma-shared.conf');
+
+module.exports = function(config) {
+  var conf = sharedConfig();
+
+  conf.files = conf.files.concat([
+    //extra testing code
+    'node_modules/angular-mocks/angular-mocks.js',
+
+    //test files
+    './test/unit/**/*.js'
+  ]);
+
+  config.set(conf);
+};
